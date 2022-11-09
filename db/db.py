ce26c0bd71a2295c3371441ff3ee168955095fc5
Ilnur <ilnrzakirov@gmail.com>
2022-11-09 16:44:25 +0300

    Добавил модели корзины и продукта

--- db/db.py
+++ db/db.py
@@ -1,12 +1,14 @@
 import datetime
 
+import sqlalchemy
 from sqlalchemy import (
     VARCHAR,
     Column,
     Date,
-    Integer,
+    Integer, ForeignKey,
 )
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 BaseModel = declarative_base()
 
@@ -14,6 +16,7 @@ BaseModel = declarative_base()
 class Groups(BaseModel):
 
     __tablename__ = "groups"
+
     id = Column(Integer, primary_key=True)
     iiko_id = Column(VARCHAR(500), nullable=False)
     name = Column(VARCHAR(500), nullable=False)
@@ -24,3 +27,28 @@ class Groups(BaseModel):
     def __init__(self, iiko_id: str, name: str):
         self.name = name
         self.iiko_id = iiko_id
+
+
+association = sqlalchemy.Table(
+    "association", BaseModel.metadata,
+    Column("product_id", ForeignKey("products.id")),
+    Column("basket_id", ForeignKey("baskets.id")),
+)
+
+
+class Product(BaseModel):
+
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(VARCHAR(300), nullable=False)
+    group = Column(Integer, ForeignKey("groups.id"))
+
+
+class Basket(BaseModel):
+
+    __tablemane__ = "baskets"
+
+    id = Column(Integer, primary_key=True)
+    chat_id = Column(Integer, nullable=False)
+    products = relationship("Product", secondary="association")
