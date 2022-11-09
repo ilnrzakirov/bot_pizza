91f347974877f8787e2581a831e6b67c4b691de7
Ilnur <ilnrzakirov@gmail.com>
2022-11-09 15:33:31 +0300

    Добавил callback

--- handlers/client.py
+++ handlers/client.py
@@ -1,8 +1,9 @@
 from aiogram import Dispatcher, types
-from aiogram.types import ContentType
+from aiogram.types import ContentType, CallbackQuery
 from loguru import logger
 
 from keyboards.clients import get_menu_button
+from settings import groups
 
 
 async def menu(message: types.Message):
@@ -32,6 +33,9 @@ async def help_menu(message: types.Message):
     logger.info(f"Получена команда {message.text} от {message.from_user.username} - id {message.from_user.id}")
     await message.answer("Что то есть")
 
+async def get_group_items(call: CallbackQuery):
+    logger.info(f"Получена команда {call.data} от {call.from_user.username} - id {call.from_user.id}")
+    await call.message.answer(text="Ok")
 
 def register_handlers_client(dispatcher: Dispatcher):
     dispatcher.register_message_handler(menu, text="🍽 Меню", content_types=ContentType.TEXT)
@@ -40,3 +44,4 @@ def register_handlers_client(dispatcher: Dispatcher):
     dispatcher.register_message_handler(basket, text="🛒 Корзина", content_types=ContentType.TEXT)
     dispatcher.register_message_handler(locations, text="📍 Как нас найти", content_types=ContentType.TEXT)
     dispatcher.register_message_handler(help_menu, text="⚙ Помощь", content_types=ContentType.TEXT)
+    dispatcher.register_callback_query_handler(get_group_items, lambda call: call.data in groups.keys(),)
